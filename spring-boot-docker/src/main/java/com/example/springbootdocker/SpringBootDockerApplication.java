7c8605d4310249e23d2f74f12aa832939662cf2f
J Matthew Gasataya <jmatthewgasataya@gmail.com>
2024-04-18 13:40:55 +0800

    v4

--- spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDockerApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World! v3";
+		return "Hello World! v4";
 	}
 
 	public static void main(String[] args) {
